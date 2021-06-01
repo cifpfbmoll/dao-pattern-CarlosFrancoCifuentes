7b2ff18128feaad3a8cf8171f9cb158ca70049d8
CarlosFrancoCifuentes <72622430+CarlosFrancoCifuentes@users.noreply.github.com>
2021-06-01 18:11:34 +0200

    [IMP]: Creación de entidad, tablas y columnas

--- src/main/java/org/pingpong/restjson/Fruit.java
+++ src/main/java/org/pingpong/restjson/Fruit.java
@@ -1,17 +1,32 @@
 package org.pingpong.restjson;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-@JsonPropertyOrder({"name", "decription"})
+@Entity
+@Table(name = "Fruit")
+@JsonPropertyOrder({ "name", "decription" })
 public class Fruit {
 
     // los propiedades han de ser publicas para que jackson
     // pueda acceder a ellar por reflection
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
     @NotBlank
     private String name;
+
+    @Column
     @NotEmpty
     public String description;
 
@@ -26,16 +41,8 @@ public class Fruit {
     public String getName() {
         return this.name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
-
-    /**
-    substituit getName por este metodo en
-    la serializacion a JSON
-    @JsonGetter("name")
-    public String nombre() {
-        return "UMAMI";
-    }*/
-
 }
